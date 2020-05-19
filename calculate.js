6e1f22eeae9c76238baafd4416f5aadc48daa679
ResetMind <avmakarov.email@gmail.com>
2020-05-19 21:25:51 +0300

    calculate

--- calculate.js
+++ calculate.js
@@ -1,3 +1,9 @@
+let table_header = document.querySelector("#table3_header");
+let invisible_table_header = document.querySelector("#invisible_table3_header");
+invisible_table_header.innerHTML = table_header.innerHTML;
+let table = document.querySelectorAll("#table3");
+let content = document.querySelector(".content3");
+
 function find() {
     let year1 = search.year1.value;
     let year2 = search.year2.value;
@@ -9,8 +15,16 @@ function find() {
     request.addEventListener("readystatechange", () => {
         if (request.readyState === 4 && request.status === 200) {
             let response = request.responseText;
-            console.log(response);
+            if(response.includes("tbody")) {
+                table[2].innerHTML = response;
+            } else {
+                table[2].innerHTML = "<tbody><tr><td>Совпадений нет<td></tr></tbody>";
+            }
         }
     });
     request.send();
 }
+
+content.onscroll = function() {
+    invisible_table_header.style.left = (-content.scrollLeft + 5) + "px";
+};
--- find.php
+++ find.php
@@ -13,7 +13,9 @@ function find($link) {
 	$arr = array();
 	if ($_GET['year1'] != "") {
 		$arr[] = "year >= " . $_GET['year1'];
-	}
+	} else {
+        $arr[] = "year >= 0";
+    }
 	if ($_GET['year2'] != "") {
 		$arr[] = "year <= " . $_GET['year2'];
 	}
@@ -37,49 +39,137 @@ function find($link) {
 		}
 	}
 	$rows = "SELECT * from factors where " . $request;
-	if (!$result = mysqli_query($link, $rows)) {
+	if (!$rows_result = mysqli_query($link, $rows)) {
 		echo "Не могу выполнить запрос.";
 		exit();
 	}
-	if ($row = mysqli_fetch_assoc($result)) {
+	if ($row = mysqli_fetch_assoc($rows_result)) {
 		$result = "<tbody>";
-		$result .= "<tr>";
-		$result .= "<th>id</th>";
-		$result .= "<th>Название_фильма</th>";
-		$result .= "<th>Год_выхода</th>";
-		$result .= "<th>Страна</th>";
-		$result .= "<th>Режиссер</th>";
-		$result .= "<th>Сценарист</th>";
-		$result .= "<th>Актеры</th>";
-		$result .= "<th>Бюджет</th>";
-		$result .= "<th>Сборы</th>";
-		$result .= "<th>Дата_выхода</th>";
-		$result .= "<th>Рейтинг</th>";
-		$result .= "</tr>";
 		do {
 			$result .= "<tr>";
-			$result .= "<td>" . $top_row['id'] . "</td>";
-			$result .= "<td>" . $top_row['Название_фильма'] . "</td>";
-			$result .= "<td>" . $top_row['Год_выхода'] . "</td>";
-			$result .= "<td>" . $top_row['Страна'] . "</td>";
-			$result .= "<td>" . $top_row['Режиссер'] . "</td>";
-			$result .= "<td>" . $top_row['Сценарист'] . "</td>";
-			$arr = explode("\n", $top_row['Актеры']);
-			$result .= "<td>";
-			for ($i = 0; $i < count($arr); $i++) {
-				$result .= "<span id=\"actors\">" . $arr[$i] . "</span><br/>";
-			}
-			$result .= "</td>";
-			$result .= "<td>" . $top_row['Бюджет'] . "</td>";
-			$result .= "<td>" . $top_row['Сборы'] . "</td>";
-			$result .= "<td>" . $top_row['Дата_выхода'] . "</td>";
-			$result .= "<td>" . $top_row['Рейтинг'] . "</td>";
-			$result .= "</tr>";
-		} while ($top_row = mysqli_fetch_assoc($top_result));
+            $result .= "<td>". $row['year']."</td>";
+            $result .= "<td>". $row['sumO']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X1</td>";
+            $result .= "<td>". $row['sumO']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X2</td>";
+            $result .= "<td>". $row['sumO']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X3</td>";
+            $result .= "<td>". $row['sumO']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X4</td>";
+            $result .= "<td>". $row['sumO2']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X5</td>";
+            $result .= "<td>". $row['sumO2']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X6</td>";
+            $result .= "<td>". $row['sumO2']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X7</td>";
+            $result .= "<td>". $row['sumO2']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X8</td>";
+            $result .= "<td>". $row['sumB']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X9</td>";
+            $result .= "<td>". $row['sumB']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X10</td>";
+            $result .= "<td>". $row['sumB']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X11</td>";
+            $result .= "<td>". $row['sumB']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X12</td>";
+            $result .= "<td>". $row['sumB40']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X13</td>";
+            $result .= "<td>". $row['sumB45']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X14</td>";
+            $result .= "<td>". $row['sumB50']."</td>";
+            $result .= "<td>". $row['sumT']."</td>";
+            $result .= "<td>X15</td>";
+            $result .= "<td>". $row['sumB40']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X16</td>";
+            $result .= "<td>". $row['sumB40']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X17</td>";
+            $result .= "<td>". $row['sumB40']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X18</td>";
+            $result .= "<td>". $row['sumB45']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X19</td>";
+            $result .= "<td>". $row['sumB45']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X20</td>";
+            $result .= "<td>". $row['sumB45']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X21</td>";
+            $result .= "<td>". $row['sumB50']."</td>";
+            $result .= "<td>". $row['sumT10']."</td>";
+            $result .= "<td>X22</td>";
+            $result .= "<td>". $row['sumB50']."</td>";
+            $result .= "<td>". $row['sumT15']."</td>";
+            $result .= "<td>X23</td>";
+            $result .= "<td>". $row['sumB50']."</td>";
+            $result .= "<td>". $row['sumT20']."</td>";
+            $result .= "<td>X24</td>";
+            $result .= "<td>". $row['chdO']."</td>";
+            $result .= "<td>". $row['chdT10']."</td>";
+            $result .= "<td>X25</td>";
+            $result .= "<td>". $row['chdO']."</td>";
+            $result .= "<td>". $row['chdT15']."</td>";
+            $result .= "<td>X26</td>";
+            $result .= "<td>". $row['chdO']."</td>";
+            $result .= "<td>". $row['chdT20']."</td>";
+            $result .= "<td>X27</td>";
+            $result .= "<td>". $row['chdO2']."</td>";
+            $result .= "<td>". $row['chdT10']."</td>";
+            $result .= "<td>X28</td>";
+            $result .= "<td>". $row['chdO2']."</td>";
+            $result .= "<td>". $row['chdT15']."</td>";
+            $result .= "<td>X29</td>";
+            $result .= "<td>". $row['chdO2']."</td>";
+            $result .= "<td>". $row['chdT20']."</td>";
+            $result .= "<td>X30</td>";
+            $result .= "<td>". $row['chdB40']."</td>";
+            $result .= "<td>". $row['chdT10']."</td>";
+            $result .= "<td>X31</td>";
+            $result .= "<td>". $row['chdB40']."</td>";
+            $result .= "<td>". $row['chdT15']."</td>";
+            $result .= "<td>X32</td>";
+            $result .= "<td>". $row['chdB40']."</td>";
+            $result .= "<td>". $row['chdT20']."</td>";
+            $result .= "<td>X33</td>";
+            $result .= "<td>". $row['chdB45']."</td>";
+            $result .= "<td>". $row['chdT10']."</td>";
+            $result .= "<td>X34</td>";
+            $result .= "<td>". $row['chdB45']."</td>";
+            $result .= "<td>". $row['chdT15']."</td>";
+            $result .= "<td>X35</td>";
+            $result .= "<td>". $row['chdB45']."</td>";
+            $result .= "<td>". $row['chdT20']."</td>";
+            $result .= "<td>X36</td>";
+            $result .= "<td>". $row['chdB50']."</td>";
+            $result .= "<td>". $row['chdT10']."</td>";
+            $result .= "<td>X37</td>";
+            $result .= "<td>". $row['chdB50']."</td>";
+            $result .= "<td>". $row['chdT15']."</td>";
+            $result .= "<td>X38</td>";
+            $result .= "<td>". $row['chdB50']."</td>";
+            $result .= "<td>". $row['chdT20']."</td>";
+            $result .= "<td>X39</td>";
+            $result .= "</tr>";
+		} while ($row = mysqli_fetch_assoc($rows_result));
 		$result .= "</tbody>";
 		echo $result;
 	} else {
-		echo "Совпадений не найдено.<br/>";
+		echo "Совпадений не найдено.";
 	}
 }
-?>
