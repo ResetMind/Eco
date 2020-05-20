5791c809905ddf8e03ae25b736fa7cfabc127e8a
ResetMind <avmakarov.email@gmail.com>
2020-05-20 15:18:30 +0300

    calculate

--- calculate.js
+++ calculate.js
@@ -3,6 +3,8 @@ let invisible_table_header = document.querySelector("#invisible_table3_header");
 invisible_table_header.innerHTML = table_header.innerHTML;
 let table = document.querySelectorAll("#table3");
 let content = document.querySelector(".content3");
+let headers = document.querySelectorAll("#invisible_table3_header th");
+let cells = document.querySelectorAll("#table3_body td");
 
 function find() {
     let year1 = search.year1.value;
@@ -15,8 +17,9 @@ function find() {
     request.addEventListener("readystatechange", () => {
         if (request.readyState === 4 && request.status === 200) {
             let response = request.responseText;
-            if(response.includes("tbody")) {
+            if (response.includes("tbody")) {
                 table[2].innerHTML = response;
+                cellOnFocus();
             } else {
                 table[2].innerHTML = "<tbody><tr><td>Совпадений нет<td></tr></tbody>";
             }
@@ -25,6 +28,271 @@ function find() {
     request.send();
 }
 
+function calc() {
+    let cells = document.querySelectorAll("#table3_body td");
+    let headers = document.querySelectorAll("#invisible_table3_header th");
+    for (let i = 0; i < cells.length; i++) {
+        let col = i % headers.length;
+        if (col % 3 == 0 && col != 0) {
+            let a = parseFloat(+(cells[i - 2].innerHTML));
+            let b = parseFloat(+(cells[i - 1].innerHTML));
+            if (isNaN(a) || isNaN(b)) {
+                continue;
+            }
+            if (col == 3) {
+                cells[i].innerHTML = x1(a, b).toFixed(3);
+            } else if (col == 6) {
+                cells[i].innerHTML = x2(a, b).toFixed(3);
+            } else if (col == 9) {
+                cells[i].innerHTML = x3(a, b).toFixed(3);
+            } else if (col == 12) {
+                cells[i].innerHTML = x4(a, b).toFixed(3);
+            } else if (col == 15) {
+                cells[i].innerHTML = x5(a, b).toFixed(3);
+            } else if (col == 18) {
+                cells[i].innerHTML = x6(a, b).toFixed(3);
+            } else if (col == 21) {
+                cells[i].innerHTML = x7(a, b).toFixed(3);
+            } else if (col == 24) {
+                cells[i].innerHTML = x8(a, b).toFixed(3);
+            } else if (col == 27) {
+                cells[i].innerHTML = x9(a, b).toFixed(3);
+            } else if (col == 30) {
+                cells[i].innerHTML = x10(a, b).toFixed(3);
+            } else if (col == 33) {
+                cells[i].innerHTML = x11(a, b).toFixed(3);
+            } else if (col == 36) {
+                cells[i].innerHTML = x12(a, b).toFixed(3);
+            } else if (col == 39) {
+                cells[i].innerHTML = x13(a, b).toFixed(3);
+            } else if (col == 42) {
+                cells[i].innerHTML = x14(a, b).toFixed(3);
+            } else if (col == 45) {
+                cells[i].innerHTML = x15(a, b).toFixed(3);
+            } else if (col == 48) {
+                cells[i].innerHTML = x16(a, b).toFixed(3);
+            } else if (col == 51) {
+                cells[i].innerHTML = x17(a, b).toFixed(3);
+            } else if (col == 54) {
+                cells[i].innerHTML = x18(a, b).toFixed(3);
+            } else if (col == 57) {
+                cells[i].innerHTML = x19(a, b).toFixed(3);
+            } else if (col == 60) {
+                cells[i].innerHTML = x20(a, b).toFixed(3);
+            } else if (col == 63) {
+                cells[i].innerHTML = x21(a, b).toFixed(3);
+            } else if (col == 66) {
+                cells[i].innerHTML = x22(a, b).toFixed(3);
+            } else if (col == 69) {
+                cells[i].innerHTML = x23(a, b).toFixed(3);
+            } else if (col == 72) {
+                cells[i].innerHTML = x24(a, b).toFixed(3);
+            } else if (col == 75) {
+                cells[i].innerHTML = x25(a, b).toFixed(3);
+            } else if (col == 78) {
+                cells[i].innerHTML = x26(a, b).toFixed(3);
+            } else if (col == 81) {
+                cells[i].innerHTML = x27(a, b).toFixed(3);
+            } else if (col == 84) {
+                cells[i].innerHTML = x28(a, b).toFixed(3);
+            } else if (col == 87) {
+                cells[i].innerHTML = x29(a, b).toFixed(3);
+            } else if (col == 90) {
+                cells[i].innerHTML = x30(a, b).toFixed(3);
+            } else if (col == 93) {
+                cells[i].innerHTML = x31(a, b).toFixed(3);
+            } else if (col == 96) {
+                cells[i].innerHTML = x32(a, b).toFixed(3);
+            } else if (col == 99) {
+                cells[i].innerHTML = x33(a, b).toFixed(3);
+            } else if (col == 102) {
+                cells[i].innerHTML = x34(a, b).toFixed(3);
+            } else if (col == 105) {
+                cells[i].innerHTML = x35(a, b).toFixed(3);
+            } else if (col == 108) {
+                cells[i].innerHTML = x36(a, b).toFixed(3);
+            } else if (col == 111) {
+                cells[i].innerHTML = x37(a, b).toFixed(3);
+            } else if (col == 114) {
+                cells[i].innerHTML = x38(a, b).toFixed(3);
+            } else if (col == 117) {
+                cells[i].innerHTML = x39(a, b).toFixed(3);
+            }
+        }
+    }
+}
+
 content.onscroll = function() {
     invisible_table_header.style.left = (-content.scrollLeft + 5) + "px";
 };
+
+function cellOnFocus() {
+    cells = document.querySelectorAll("#table3_body td");
+    headers = document.querySelectorAll("#invisible_table3_header th");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].onfocus = function(e) {
+            let row = parseInt(i / headers.length);
+            table[2].querySelector("tr:nth-child(" + (row + 1) + ")").style.background = "#2a2a2a";
+        }
+        cells[i].onblur = function(e) {
+            let row = parseInt(i / headers.length);
+            table[2].querySelector("tr:nth-child(" + (row + 1) + ")").style.background = "transparent";
+        }
+    }
+}
+
+function x1(so, st) {
+    return so * 10 / st;
+}
+
+function x2(so, st10) {
+    return so * 10 / st10;
+}
+
+function x3(so, st15) {
+    return so * 10 / st15;
+}
+
+function x4(so, st20) {
+    return so * 10 / (st20 + 20);
+}
+
+function x5(so2, st) {
+    return so2 * 10 / st;
+}
+
+function x6(so2, st10) {
+    return so2 * 10 / st10;
+}
+
+function x7(so2, st15) {
+    return so2 * 10 / st15;
+}
+
+function x8(so2, st20) {
+    return so2 * 10 / (st20 + 20);
+}
+
+function x9(sv, st) {
+    return sv / st;
+}
+
+function x10(sv, st10) {
+    return sv / st10;
+}
+
+function x11(sv, st15) {
+    return sv / st15;
+}
+
+function x12(sv, st20) {
+    return sv / (st20 + 20);
+}
+
+function x13(sv40, st) {
+    return sv40 / st;
+}
+
+function x14(sv45, st) {
+    return sv45 / st;
+}
+
+function x15(sv50, st) {
+    return sv50 / st;
+}
+
+function x16(sv40, st10) {
+    return sv40 / st10;
+}
+
+function x17(sv40, st15) {
+    return sv40 / st15;
+}
+
+function x18(sv40, st20) {
+    return sv40 / (st20 + 20);
+}
+
+function x19(sv45, st10) {
+    return sv45 / st10;
+}
+
+function x20(sv45, st15) {
+    return sv45 / st15;
+}
+
+function x21(sv45, st20) {
+    return sv45 / (st20 + 20);
+}
+
+function x22(sv50, st10) {
+    return sv50 / st10;
+}
+
+function x23(sv50, st15) {
+    return sv50 / st15;
+}
+
+function x24(sv50, st20) {
+    return sv50 / (st20 + 20);
+}
+
+function x25(chdo, chdt10) {
+    return chdo / chdt10;
+}
+
+function x26(chdo, chdt15) {
+    return chdo / chdt15;
+}
+
+function x27(chdo, chdt20) {
+    return chdo / (chdt20 + 1);
+}
+
+function x28(chdo2, chdt10) {
+    return chdo2 / chdt10;
+}
+
+function x29(chdo2, chdt15) {
+    return chdo2 / chdt15;
+}
+
+function x30(chdo2, chdt20) {
+    return chdo2 / (chdt20 + 1);
+}
+
+function x31(chdv40, chdt10) {
+    return chdv40 / chdt10;
+}
+
+function x32(chdv40, chdt15) {
+    return chdv40 / chdt15;
+}
+
+function x33(chdv40, chdt20) {
+    return chdv40 / (chdt20 + 1);
+}
+
+function x34(chdv45, chdt10) {
+    return chdv45 / chdt10;
+}
+
+function x35(chdv45, chdt15) {
+    return chdv45 / chdt15;
+}
+
+function x36(chdv45, chdt20) {
+    return chdv45 / (chdt20 + 1);
+}
+
+function x37(chdv50, chdt10) {
+    return chdv50 / chdt10;
+}
+
+function x38(chdv50, chdt15) {
+    return chdv50 / chdt15;
+}
+
+function x39(chdv50, chdt20) {
+    return chdv50 / (chdt20 + 1);
+}
