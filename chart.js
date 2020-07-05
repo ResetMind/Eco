5434632ecabfee19c89e8f1b5ab3dccad78ed3e9
ResetMind <avmakarov.email@gmail.com>
2020-07-05 20:32:31 +0300

    checkboxes

--- chart.js
+++ chart.js
@@ -6,8 +6,8 @@ let chart_error = document.querySelector("#chart_error");
 let chart_holder = document.querySelector("#chart_holder");
 let used_charts = document.querySelector(".used_charts");
 let add_button = document.querySelector(".add");
-let x_arr_sorted, y_arr_sorted, year_arr_sorted, new_win, data = new Array(),
-    years = new Array();
+let x_arr_sorted, y_arr_sorted, year_arr_sorted, new_win, data = new Array(), data2 = new Array(),
+    years = new Array(), div_index;
 let headers = window.opener.document.querySelectorAll("#invisible_table3_header th");
 let cells = window.opener.document.querySelectorAll("#table3_body td");
 let layout = { showlegend: true };
@@ -15,6 +15,7 @@ let modal = document.querySelector(".modal");
 let modal_body = document.querySelector(".modal_body");
 let close_modal_btn = document.querySelector(".close_modal");
 let overlay = document.querySelector(".overlay");
+let checkboxes;
 newPlot();
 
 add_button.addEventListener("click", function() {
@@ -48,15 +49,11 @@ function createDeleteLisneners() {
         delete_chart_buttons[i].onclick = function f() {
             used_charts.childNodes[i].removeEventListener("click", openModal);
             used_charts.childNodes[i].remove();
-            refreshCharts(i);
+            refreshChartsAfterDelete(i);
             delete_chart_buttons[i].removeEventListener("click", f);
             createDeleteLisneners();
         }
     }
-}
-
-function createOpenModalListeners() {
-
 }
 
 function addChart(chart_name) {
@@ -67,9 +64,11 @@ function addChart(chart_name) {
             y: y_arr_sorted,
             type: "scatter",
             mode: "lines+markers",
+            connectgaps: true,
             name: chart_name
         };
         data.push(trace1);
+        data2.push(trace1);
         years.push(year_arr_sorted);
         newPlot();
         return true;
@@ -79,8 +78,9 @@ function addChart(chart_name) {
     }
 }
 
-function refreshCharts(i) {
+function refreshChartsAfterDelete(i) {
     data.splice(i, 1);
+    data2.splice(i, 1);
     years.splice(i, 1);
     newPlot();
 }
@@ -169,17 +169,18 @@ function openModal() {
             break;
         }
     }
+    div_index = i;
     let chart_info = div.querySelector(".chart_info").innerHTML;
     let x_name = chart_info.split(" и ")[0];
     let y_name = chart_info.split(" и ")[1];
     let inner_html = "<table id=\"table_modal\" align=\"center\"><thead><tr><th></th>";
-    if(x_name != "Год") {
+    if(x_name != "Год" && y_name != "Год") {
         inner_html += "<td>Год</td>";
     }
     inner_html += "<td>" + x_name + "</td>" + "<td>" + y_name + "</td></tr></thead><tbody>";
     for(let k = 0; k < data[i].x.length; k++) {
-        inner_html += "<tr><td><input type=\"checkbox\" checked></td>";
-        if(x_name != "Год") {
+        inner_html += "<tr><td><input type=\"checkbox\"></td>";
+        if(x_name != "Год" && y_name != "Год") {
             inner_html += "<td>" + years[i][k] + "</td>";
         }
         inner_html += "<td>" + data[i].x[k] + "</td>";
@@ -187,11 +188,37 @@ function openModal() {
     }
     inner_html += "</tbody></table>";
     modal_body.innerHTML = inner_html;
+    checkboxes = modal_body.querySelectorAll("input[type=\"checkbox\"]");
+    for(let k = 0; k < checkboxes.length; k++) {
+        if(!data[i].x[k].includes("span")) {
+            checkboxes[k].checked = true;
+        } else {
+            checkboxes[k].checked = false;
+        }
+        checkboxes[k].addEventListener("change", onCheckboxChange);
+    }
+}
+
+function onCheckboxChange() {
+    for(let k = 0; k < checkboxes.length; k++) {
+        if(checkboxes[k].checked) {
+            data[div_index].x[k] = data[div_index].x[k].replace("<span>", "");
+            data[div_index].x[k] = data[div_index].x[k].replace("</span>", "");
+        } else if(!data[div_index].x[k].includes("span")){
+            data[div_index].x[k] = "<span>" + data[div_index].x[k] + "</span>";
+        }
+        console.log(data[div_index].x[k]);
+    }
+    console.log("-----------------------");
+    newPlot();
 }
 
 function closeModal() {
     modal.classList.remove('active');
     overlay.classList.remove('active');
+    for(let k = 0; k < checkboxes.length; k++) {
+        checkboxes[k].removeEventListener("change", onCheckboxChange);
+    }
 }
 
 close_modal_btn.onclick = function() {
